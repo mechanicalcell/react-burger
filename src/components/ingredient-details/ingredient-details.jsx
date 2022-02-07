5b7399129b487d186f1bc9a70b13944a28e74eae
ViacheslavMe <92511058+ViacheslavMe@users.noreply.github.com>
2022-02-08 01:53:02 +0800

    useEffect/newArray

--- src/components/ingredient-details/ingredient-details.jsx
+++ src/components/ingredient-details/ingredient-details.jsx
@@ -1,20 +1,31 @@
 import styles from '../ingredient-details/ingredient-details.module.css';
 import { useState } from 'react';
+import { useEffect } from 'react';
 
 export default function IngredientDetails({data, onOpen}) {
   const [isMem, setIsMem] = useState({ newArr: [] });
 
+useEffect(()=>{
+  newArray();
+  return () => {
+    document.removeEventListener("click", newArray);
+  }
+}, [])  
+
 const ingrArr = Array.from(document.querySelectorAll('article'))
+
+const newArray = () => {
+  ingrArr.map(ing => 
+    ing.addEventListener('click', e => { 
+      if (ing.querySelectorAll('.name')[0].innerHTML == data.name) {
+        return setIsMem({newArr: data})  
+      }   
+    })) 
+}
 
 return (
   <div> 
-    {ingrArr.map(ing => 
-      ing.addEventListener('click', e => { 
-        if (ing.querySelectorAll('.name')[0].innerHTML == data.name) {
-          return setIsMem({newArr: data})  
-        }   
-      })) 
-    } 
+    
     <img src={isMem.newArr.image} alt={isMem.newArr.name} />
     <p className={styles.name_modal}>{isMem.newArr.name}</p>
     {isMem.newArr.name && 
