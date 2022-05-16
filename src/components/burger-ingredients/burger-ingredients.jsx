c8c3523a9ccff2bbf12a0b7059e02b62a04fe609
MechaniCell <92511058+MechaniCell@users.noreply.github.com>
2022-05-16 13:13:36 +0800

    inline/LinkStyle

--- src/components/burger-ingredients/burger-ingredients.jsx
+++ src/components/burger-ingredients/burger-ingredients.jsx
@@ -46,8 +46,8 @@ const image = (
 return (item.type === ingrType && 
 (<Link to={{
   pathname: `/ingredients/${item._id}`,
-  state: { background: location }
-}} ref={dragRef} style={{ position: "relative" }} onClick={onClick} className={`${styles.ingredients} mb-8`}>
+  state: { background: location } 
+}} ref={dragRef} onClick={onClick} className={`${styles.ingredients} mb-8`}>
   {count[index] > 0 && (<Counter count={count[index]} size="default" />)} 
   <div className='ml-4 mr-4'>{image}</div> 
   <div className={`${styles.price_and_icon} text text_type_digits-default`}>{item.price}
